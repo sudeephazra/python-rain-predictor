0458f5bcf685fe469debdc945eb0eee58b12fb6f
Sudeep Hazra <sudeephazra@gmail.com>
2018-12-03 01:06:20 +0530

    Update python-rain-predictor.py

--- python-rain-predictor/python-rain-predictor.py
+++ python-rain-predictor/python-rain-predictor.py
@@ -35,20 +35,46 @@ data_cleansed = data[['WeatherType', 'DryBulbCelsius', 'RelativeHumidity']]
 data_cleansed.columns = ['isRain', 'temperature', 'humidity']
 # 4. Replace values if required
 pandas.options.mode.chained_assignment = None  # default='warn'
-data_cleansed['isRain'].replace(to_replace=r'-RA|-SN|-DZ|-PL', value='Yes', regex=True, inplace=True)
-data_cleansed['isRain'].replace(to_replace=r'\s+', value='No', regex=True, inplace=True)
+data_cleansed['isRain'].replace(to_replace=r'-(RA|SN|DN|PL).*$', value=r'Yes', regex=True, inplace=True)
+data_cleansed['isRain'].replace(to_replace=r'^(?!.*Yes).*$', value=r'No', regex=True, inplace=True)
 # print(data_cleansed.shape)
 # print(data_cleansed)
 
 
-X = data_cleansed.iloc[:,2:]
-y = data_cleansed.iloc[:,1]
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-print(X_train.shape)
-classifier = LogisticRegression(random_state=0)
-classifier.fit(X_train, y_train)
-y_pred = classifier.predict(X_test)
-from sklearn.metrics import confusion_matrix
-confusion_matrix = confusion_matrix(y_test, y_pred)
-print(confusion_matrix)
-print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(classifier.score(X_test, y_test)))
+# x = data_cleansed.iloc[:, 2:]
+# y = data_cleansed.iloc[:, 1:]
+# X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=0, test_size=0.25)
+# print(X_train.shape)
+# print(y)
+# classifier = LogisticRegression(random_state=0)
+# classifier.fit(X_train, y_train)
+
+# y_pred = classifier.predict(X_test)
+# from sklearn.metrics import confusion_matrix
+# confusion_matrix = confusion_matrix(y_test, y_pred)
+# print(confusion_matrix)
+# print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(classifier.score(X_test, y_test)))
+
+# from sklearn import svm, datasets
+# from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix #for using confusion matrix###
+#
+logistic = LogisticRegression()
+logistic.fit(data_cleansed[['temperature']+['humidity']], data_cleansed[['isRain']])
+predict = logistic.predict(data_cleansed[['temperature']+['humidity']])
+# Use score method to get accuracy of model
+cm1 = confusion_matrix(data_cleansed[['isRain']], predict)
+print(cm1)
+total1 = sum(sum(cm1))
+# print(total1)
+accuracy1 = (cm1[0, 0] + cm1[1, 1] + cm1[2, 2] + cm1[3, 3] + cm1[4, 4] + cm1[5, 5])/total1
+print(accuracy1)
+
+# plt.figure(figsize=(9,9))
+# sns.heatmap(cm1, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r');
+# plt.ylabel('Actual label');
+# plt.xlabel('Predicted label');
+# all_sample_title = 'Accuracy Score: {0}'.format(score)
+# plt.title(all_sample_title, size = 15);
+
+
