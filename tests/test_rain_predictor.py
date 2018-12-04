70fadd1dcff8cc3b2148d8875292027f6b3eef71
Sudeep Hazra <sudeephazra@gmail.com>
2018-12-04 17:53:40 +0530

    Update test_rain_predictor.py

--- tests/test_rain_predictor.py
+++ tests/test_rain_predictor.py
@@ -8,8 +8,8 @@ def test_rain_predictor_file_read():
     rain_predictor = RainPredictor.RainPredictor('../data/Weather Dataset_Filtered.csv')
     dataframe = rain_predictor.read_file(column_indices=[8, 10, 15])
     dataframe = rain_predictor.rename_columns(dataframe, ['isRain', 'temperature', 'humidity'])
-    dataframe = rain_predictor.replace_data_regex(dataframe, 'isRain', re.compile('^.*(RA|SN|DN|PL).*$'), 'Yes')
-    dataframe = rain_predictor.replace_data_regex(dataframe, 'isRain', re.compile('^(?!.*Yes).*$'), 'No')
+    dataframe = rain_predictor.replace_data(dataframe, 'isRain', re.compile('^.*(RA|SN|DN|PL).*$'), 'Yes')
+    dataframe = rain_predictor.replace_data(dataframe, 'isRain', re.compile('^(?!.*Yes).*$'), 'No')
     # Display the unique list of test data of probable predicted values
     # sns.countplot(y=dataframe['isRain'], data=dataframe)
     # plt.show()
@@ -18,4 +18,5 @@ def test_rain_predictor_file_read():
     # c_matrix = rain_predictor.get_confusion_matrix(logistic, x_test, y_test)
     # rain_predictor.display_confusion_matrix(c_matrix)
     print(rain_predictor.get_rain_prediction(logistic, [[24.4, 90]]))
+
 
